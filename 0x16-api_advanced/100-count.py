e4b58b6acded650e1c120be90cb7d1ab9543b195
NomkhosiDube <nomkhosidube2@gmail.com>
2024-05-09 03:26:11 -0700

    0x16-api_advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,7 +5,7 @@
 import requests
 
 
-def count_words(subreddit, word_list, after='', word_dict={}):
+def count_words(subreddit, word_list):
     """ A function that queries the Reddit API parses the title of
     all hot articles, and prints a sorted count of given keywords
     (case-insensitive, delimited by spaces.
